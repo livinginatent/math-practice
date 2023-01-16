a0f3049c83ae2421fdcf598ed9423cae18e26c46
livinginatent <suleyman.eminbeyli@gmail.com>
2023-01-16 21:19:55 +0400

    basic arithmetics completed

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -142,7 +142,7 @@ export default function SignIn() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="http://localhost:3000/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
