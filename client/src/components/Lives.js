78a3e88c45a67bc5f49561a57b141b95e55af661
livinginatent <suleyman.eminbeyli@gmail.com>
2023-02-11 11:10:22 +0400

    tested Lives component

--- client/src/components/Lives.js
+++ client/src/components/Lives.js
@@ -4,27 +4,25 @@ import { useSelector } from "react-redux";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 
-
 function Lives() {
   const lives = useSelector((state) => state.game.lives);
-  
+
   let hearts = [];
-
-
 
   if (lives < 5) {
     for (let i = 0; i < lives; i++) {
       hearts.push(<FavoriteIcon fontSize="large" key={i} />);
     }
   } else {
-    hearts.push(<FavoriteIcon size='large' key={lives} />);
+    hearts.push(<FavoriteIcon size="large" key={lives} />);
   }
- 
 
   return (
-    <>
-      <Box>{hearts}</Box>
-    </>
+  
+      <Box>
+        <span data-testid="hearts-state">{hearts}</span>
+      </Box>
+    
   );
 }
 
---
+++ client/src/components/Lives.test.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Lives from "./Lives";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+
+// FIX THE ESLINT ISSUE
+// FIX THE ESLINT ISSUE
+// FIX THE ESLINT ISSUE
+// FIX THE ESLINT ISSUE
+
+const initialState = {
+  game: {
+    points: 0,
+    lives: 3,
+    seconds: 20,
+    newGame: false,
+    isStarted: false,
+    isFinished: false,
+  },
+};
+
+const mockStore = configureMockStore();
+const store = mockStore(initialState);
+
+describe("Lives component", () => {
+  test('Matches the snapshot', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Lives />
+      </Provider>
+    );
+    expect(container).toMatchSnapshot()
+  })
+  test("renders 3 hearts", () => {
+    render(
+      <Provider store={store}>
+        <Lives />
+      </Provider>
+    );
+   
+ const hearts = screen.getByTestId('hearts-state')
+
+  expect(hearts.getElementsByTagName('svg')).toHaveLength(3);
+  });
+});
